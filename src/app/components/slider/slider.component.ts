15d017266b36b5a31d5503e9951cb4ecf5e678b2
Wendeley <wendeleyf@gmail.com>
2022-10-08 04:25:01 -0300

    Passando valores do array de movies do componente home para o componente slider e adicionando material icon

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+
+  @Input() items:any;
 
   constructor() { }
 
